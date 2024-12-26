8a26f27f10a2ea653500bde5cbabfb742be4242b
ritvik-raj-singh <2023ugme011@nitjsr.ac.in>
2024-12-26 20:39:24 +0530

    model training with tracking

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from networksecurity.components.data_ingetsion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig 
 from networksecurity.exception.exception import NetworkSecurityException
 import sys
@@ -21,6 +23,16 @@ if __name__=='__main__':
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("data validation completed")
         print(data_validation_artifact)
+        logging.info("data tranformation started")
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_tranformation_artifact=data_transformation.initiate_data_transformation()
+        logging.info("data transformation  completed")   
 
+        logging.info("Model Training started")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_tranformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer() 
+    
     except Exception as e:
         raise NetworkSecurityException(e,sys)
